0b37409389046ba979f82a5d8f6c3310c31ec09b
Tim Huerne <76855940+Drenhat@users.noreply.github.com>
2021-03-24 01:38:09 -0400

    images in the images folders and JS file

---
+++ pacmen.js
@@ -0,0 +1,114 @@
+
+
+var pos = 0;
+    const pacArray = [
+        ['PacMan1.png', 'PacMan2.png'],
+        ['PacMan3.png', 'PacMan4.png']
+    ];
+    var direction = 0;
+    const pacMen = []; // This array holds all the pacmen
+    const ghost = [];
+
+    function setToRandom(scale) {
+        return {
+            x: Math.random() * scale,
+            y: Math.random() * scale
+        }
+    }
+    // Factory to make a PacMan at a random position with random velocity
+    function makePac() {
+        // returns an object with random values scaled {x: 33, y: 21}
+        let velocity = setToRandom(10); // {x:?, y:?}
+        let position = setToRandom(200);
+        //console.log(position)
+        // Add image to div id = game
+        let game = document.getElementById('game');
+        let newimg = document.createElement('img');
+        newimg.style.position = 'absolute';
+        newimg.src = './images/Pacman1.png';
+        newimg.width = Math.random() * 150;
+        //
+        // set position here - whitout the string, the left property can not be changed
+        newimg.style.left = `${position.x}px`;
+        newimg.style.top = `${position.y}px`;
+
+        // add new Child image to game
+        game.appendChild(newimg);
+        // return details in an object
+        return {
+            position,
+            velocity,
+            newimg
+        }
+    }
+
+    function makeGhost() {
+        let velocity = setToRandom(10); // {x:?, y:?}
+        let position = setToRandom(200);
+        
+        // Add image to div id = game
+        let game = document.getElementById('game');
+        let newimg = document.createElement('img');
+        newimg.style.position = 'absolute';
+        newimg.src = './images/ghost.svg';
+        newimg.width = Math.random() * 150;
+        //
+        // set position here 
+        newimg.style.left = `${position.x}px`;
+        newimg.style.top = `${position.y}px`;
+        
+
+        // add new Child image to game
+        game.appendChild(newimg);
+        // return details in an object
+        return {
+            position,
+            velocity,
+            newimg
+        }
+    }
+
+    function update() {
+        //loop over pacmen array and move each one and move image in DOM
+        pacMen.forEach((item) => {
+          console.log(item.position.y)
+            checkCollisions(item)
+            item.position.x += item.velocity.x;
+            item.position.y += item.velocity.y;
+
+            item.newimg.style.left = `${item.position.x}px`;
+            item.newimg.style.top = `${item.position.y}px`;
+        })
+        ghost.forEach((item) => {
+            checkCollisions(item)
+            item.position.x += item.velocity.x;
+            item.position.y += item.velocity.y;
+
+            item.newimg.style.left = `${item.position.x}px`;
+            item.newimg.style.top = `${item.position.y}px`;
+        })
+        setTimeout(update, 20);
+    }
+
+    function checkCollisions(item) {
+        //
+        // detect collision with all walls and make pacman bounce
+        if (item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
+            item.position.x + item.velocity.x < 0) {item.velocity.x = -item.velocity.x;
+        }
+        if (item.position.y + item.velocity.y + item.newimg.height > window.innerHeight ||
+            item.position.y + item.velocity.y < 0) {item.velocity.y = -item.velocity.y};
+    }
+
+    function makeOne() {
+        pacMen.push(makePac()); // add a new PacMan
+    }
+
+    function makeOneGhost() {
+        ghost.push(makeGhost()); // add a new PacMan
+    }
+
+//don't change this line
+//if (typeof module !== 'undefined') {
+//  module.exports = { checkCollisions, update, pacMen, makeOne, makeOneGhost};
+//}
